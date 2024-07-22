806d35539e6cf8be550125c29e0d0a0e9938f338
asp <asp@aptech.com>
2024-07-22 18:52:29 +0500

    done

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  email : string = "";
+  password : string = "";
+  check : boolean = false;
+
+  myfunc() :void {
+    if(this.email == "" || this.password == "" || this.check == false){
+
+        alert("Please Fill All Fields")
+
+    }
+    else{
+      alert ("email is" + this.email + "\n" + "password is" + this.password);
+    }
+  }
+
+
+}
