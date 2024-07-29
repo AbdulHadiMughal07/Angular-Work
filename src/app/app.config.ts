c912586bfa6f84ddfb9919824a9c6a90b3b8e701
asp <asp@aptech.com>
2024-07-29 18:31:49 +0500

    done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
+import { RegisterComponent } from './register/register.component';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { FakeApiImagesComponent } from './fake-api-images/fake-api-images.component';
+import { FakeApiDataComponent } from './fake-api-data/fake-api-data.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter([
+    {path : '', redirectTo:'register',pathMatch:'full'},
+    {path : 'register',component:RegisterComponent},
+    {path : 'fake-api-image',component:FakeApiImagesComponent},
+    {path : 'fake-api-data',component:FakeApiDataComponent},
+  ]), provideClientHydration()]
 };
